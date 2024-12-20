d74df3437af2e955a2cda85b98c70ebc8f3896f0
DrewDame <drewdame@umich.edu>
2024-12-19 21:57:05 -0500

    Footer first draft done

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import '../styles/Footer.css'
+
+function Footer() {
+    return (
+        <footer id ="footer-footer">
+            <div id="footer-logo">
+                <img src="../public/logo.png" alt="IAAAT Logo"/>
+            </div>
+            <div id="footer-about">
+                <h3>About IAAAT</h3>
+                <p>I Am An Athlete, Too is a 501(c) non-profit organization blahblahblah</p>
+            </div>
+            <div id="footer-contact">
+                <h3>Contact Us</h3>
+                <p>Twitter Insta Facebook</p>
+                <p>Email</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import '../styles/Navbar.css';
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>Navbar</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
